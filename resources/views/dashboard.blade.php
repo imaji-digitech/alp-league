f54355282a35335bc0592e1265b56d6e483094ab
masif088 <mokhamadasif@gmail.com>
2023-10-31 10:21:08 +0700

    add admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,28 +32,28 @@
     <div>
         <div class="container-fluid">
             @if(auth()->user()->school_id!=null)
-                @if (auth()->user()->school->upload1!=null or auth()->user()->school->upload2!=null)
+{{--                @if (auth()->user()->school->upload1!=null or auth()->user()->school->upload2!=null)--}}
                     @livewire('dashboard-data')
                     @livewire('dashboard-user')
                     @foreach(MatchMaking::where('school1_id',auth()->user()->school_id)->orWhere('school2_id',auth()->user()->school_id)->get() as $match)
                         @livewire('match-making-update-score',['match'=>$match,'school'=>1])
                     @endforeach
-                @else
-                    <br><br><br>
-                    <h2 style="text-align: center; font-weight: normal; width: 100%">
-                        Akun Anda tidak aktif karena tidak melakukan pendaftaran.
-                        <br>
-                        sampai bertemu di <br> <b>ALP League Kabupaten 2023.</b>
-                        <br>
-                        Ikuti aktivitas kami di website
-                        <a href="https://imajisociopreneur.id" style="text-decoration: none; color: #1c3aa1">
-                            <b><i class="fa fa-globe" style="font-size: 1.8rem"></i>&nbsp; imajisociopreneur.id</b>
-                        </a> <br>
-                        dan intagram kami
-                        <a href="http://instagram.com/imajisociopreneur/" style="text-decoration: none; color: #a11c5f">
-                            <b><i class="fa fa-instagram" style="font-size: 1.8rem"></i>&nbsp; @imajisociopreneur</b>
-                        </a>
-                    </h2>
+{{--                @else--}}
+{{--                    <br><br><br>--}}
+{{--                    <h2 style="text-align: center; font-weight: normal; width: 100%">--}}
+{{--                        Akun Anda tidak aktif karena tidak melakukan pendaftaran.--}}
+{{--                        <br>--}}
+{{--                        sampai bertemu di <br> <b>ALP League Kabupaten 2023.</b>--}}
+{{--                        <br>--}}
+{{--                        Ikuti aktivitas kami di website--}}
+{{--                        <a href="https://imajisociopreneur.id" style="text-decoration: none; color: #1c3aa1">--}}
+{{--                            <b><i class="fa fa-globe" style="font-size: 1.8rem"></i>&nbsp; imajisociopreneur.id</b>--}}
+{{--                        </a> <br>--}}
+{{--                        dan intagram kami--}}
+{{--                        <a href="http://instagram.com/imajisociopreneur/" style="text-decoration: none; color: #a11c5f">--}}
+{{--                            <b><i class="fa fa-instagram" style="font-size: 1.8rem"></i>&nbsp; @imajisociopreneur</b>--}}
+{{--                        </a>--}}
+{{--                    </h2>--}}
 
                 @endif
             @else
