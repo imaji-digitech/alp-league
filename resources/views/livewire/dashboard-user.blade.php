50376ffcb4fee8447761a2f63ea880b9e6ba4091
masif088 <mokhamadasif@gmail.com>
2023-11-04 09:25:06 +0700

    add admin

--- resources/views/livewire/dashboard-user.blade.php
+++ resources/views/livewire/dashboard-user.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    @if (auth()->user()->school->upload1==null or auth()->user()->school->upload2==null)
+{{--    @if (auth()->user()->school->upload1==null or auth()->user()->school->upload2==null)--}}
     <div class="col-sm-6 col-xl-6 col-lg-6">
         <a class="btn btn-secondary" style="width: 100%"
            href="{{route('download.surat-pernyataan-dan-pendaftaran-alp-league-kabupaten-2022')}}" target="_blank">
@@ -19,6 +19,6 @@
     <div class="col-sm-6 col-xl-6 col-lg-6 m-t-25">
         <livewire:upload2/>
     </div>
-        @endif
+{{--        @endif--}}
 
 </div>
